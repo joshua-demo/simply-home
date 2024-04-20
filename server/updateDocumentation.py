57cf447fe19378fce768ca92311ba673bff15adc
Mauricio Curiel <mauriciom.curiel@gmail.com>
2024-04-20 09:26:24 -0700

    updates documentation and adds util funciton for agents to update the documentation

--- server/updateDocumentation.py
+++ server/updateDocumentation.py
@@ -1,11 +1,14 @@
-def updateDocumentation(apiEndpoint: str, bio: str, parameters: list[str], parameterBios: str, apiType="POST", returns="", returnBio=""):
+def updateDocumentation(apiEndpoint: str, bio: str, parameters: list[str], apiType="POST", returns=""):
     with open("documentation.txt", "a") as file:
         file.write(f"\n{apiType} /{apiEndpoint}\n")
         file.write(f"{bio}\n")
         file.write("Parameters:\n")
-        for i in range(len(parameters)):
-            file.write(f"{parameters[i]}: {parameterBios[i]}\n")
+        for parameter in parameters:
+            file.write(f"{parameter}\n")
         
         if apiType == "GET":
             file.write("Returns:\n")
-            file.write(f"{returns}: {returnBio}\n")
+            file.write(f"{returns}:\n")
+
+if __name__ == "__main__":
+    updateDocumentation("test", "This is a test", ["String test1: this is a test", "int test2: this is a test 2"],  apiType="GET", returns="{test3} : this is a test response")
