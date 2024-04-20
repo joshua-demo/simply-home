5a429fabcf0d6deeb493f1fce599ac76214b7d47
Mauricio Curiel <mauriciom.curiel@gmail.com>
2024-04-19 21:28:49 -0700

    resize text area

--- home-portal/app/page.tsx
+++ home-portal/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         <AnimatePresenceWrapper isVisible={!loading} className="mt-10">
           <BackgroundGradient
             className="rounded-[22px] bg-white dark:bg-zinc-900 !p-1"
-            containerClassName="min-w-100px w-48 md:w-[200px]"
+            containerClassName="min-w-100px w-48 md:w-[600px]"
           >
             <textarea
               placeholder="What would you like your smart home to do?"
