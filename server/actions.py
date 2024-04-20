2c4a3b3a3f246493561601e2f8dbaafd1e5effb1
Steven Le <stevenleusa79@yahoo.com>
2024-04-20 02:51:07 -0700

    agent will call functions if possible, otherwise say no

---
+++ server/actions.py
@@ -0,0 +1,5 @@
+def multiply(a:float, b:float):
+    """returns a * b."""
+    return a*b
+
+# dummy function for now, i just followed the docs
--- server/agents.py
+++ server/agents.py
@@ -4,6 +4,7 @@ import google.generativeai as genai
 from dotenv import load_dotenv
 from uagents import Agent, Context, Model
 
+import actions # custom functions that will work on smart home devices
 
 class TestRequest(Model):
     command: str
@@ -33,11 +34,26 @@ async def startup(ctx: Context):
 async def query_handler(ctx: Context, sender: str, _query: TestRequest):
     ctx.logger.info(_query)
     try:
+
+        # get all functions from actions.py (got this from chatgpt)
+        all_functions = [func for func in dir(actions) if callable(getattr(actions, func))]
+        functions_to_use = [getattr(actions, func) for func in all_functions if not func.startswith("__")]
+
+        ctx.logger.info(functions_to_use)
+
         # send code to 
-        model = genai.GenerativeModel('gemini-pro')
+        model = genai.GenerativeModel('gemini-pro', tools=functions_to_use)
         command = _query.command
-        response = model.generate_content(command)
-        await ctx.send(sender, Response(text=response.text))
+
+        chat = model.start_chat(enable_automatic_function_calling=True)
+        chat.send_message(command)
+
+        has_function_call = any("function_call" in str(content.parts[0]) for content in chat.history)
+
+        if has_function_call:
+            await ctx.send(sender, Response(text="successfully adjusted smart home"))
+        else:
+            await ctx.send(sender, Response(text="no suitable function was found"))
     except Exception as e:
         ctx.logger.error(f"An error occurred: {str(e)}")
         await ctx.send(sender, Response(text="fail"))
