aba76c97a16b1f07c97e28be5b522865cde0da92
Jonathan Nguyen <jonathanguven@gmail.com>
2024-04-20 14:57:34 -0700

    remove test actions

--- server/actions.py
+++ server/actions.py
@@ -76,17 +76,3 @@ def set_living_room_light(color_name:str):
         print("POST request successful:", response.status_code)
     except requests.exceptions.RequestException as e:
         print("Error making POST request:", e)
-
-def enable_lights():
-    """Turn on the lighting system."""
-    print("LIGHTBOT: Lights enabled.")
-
-
-def set_light_color(rgb_hex: str):
-    """Set the light color. Lights must be enabled for this to work."""
-    print(f"LIGHTBOT: Lights set to {rgb_hex}.")
-
-
-def stop_lights():
-    """Stop flashing lights."""
-    print("LIGHTBOT: Lights turned off.")
